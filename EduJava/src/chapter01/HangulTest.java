d3f812fb476066e838a6bd12d0cc19f398304e17
jasmin-06a07 <limyoungmi1005@gmail.com>
2025-08-12 15:12:05 +0900

    쟈스민 입니다

--- EduJava/src/chapter01/HangulTest.java
+++ EduJava/src/chapter01/HangulTest.java
@@ -3,6 +3,6 @@ package chapter01;
 public class HangulTest {
     public static void main(String[] args) {
         System.out.println("안녕하세요, 한글 출력 테스트입니다!");
-        System.out.println("쟈스민 @@");
+        System.out.println("쟈스민: jasmin-06a07 @@");
     }
 }
